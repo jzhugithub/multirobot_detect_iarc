9823a91b47f982d35f62f2c40623bdcd8e04e921
jzhugithub <mfx106825372@163.com>
2017-08-14 23:34:58 +0800

    fix bug

--- src/multirobot_detect_iarc_node.cpp
+++ src/multirobot_detect_iarc_node.cpp
@@ -183,7 +183,7 @@ public:
     
     //detect
     //HOG_descriptor_detect.detectMultiScale(src_GPU, location_detect, HitThreshold, WinStride, Size(), DetScale, 0.2, true);//gpu
-    HOG_descriptor_detect.detectMultiScale(src_3, location_detect, HitThreshold, WinStride, Size(0,0), DetScale, 0.2, true);
+    HOG_descriptor_detect.detectMultiScale(src_3, location_detect, HitThreshold, WinStride, Size(0,0), DetScale, 2.0, false);
     location_detect = resize_boxes(location_detect, src_3, detect_resize_rate);
     
     //Non-maximum suppression
